8f74ac3f65fa155c5268002918a26be60c25846f
Quaxinim <jinsolfert@gmail.com>
2024-06-04 23:43:42 -0300

    terminando o exame_mais_usado

--- exame_mais_usados.py
+++ exame_mais_usados.py
@@ -4,20 +4,29 @@ df = pd.read_excel('FaculdadeExcel.xlsx')
 
 exames = df['especialidade']
 
-
 # Adiciona as duas colunas de nome do paciente e especialidade a um novo DF
 df_paciente_atendimento = df.loc[:, ['nome do paciente', 'especialidade']]
+df_paciente_atendimento['especialidade'] = df['especialidade'].str.upper()
+
 # Remove a linha que contem um valor vazio na coluna "nome do paciente"
 df_paciente_atendimento = df_paciente_atendimento.dropna(
     subset='nome do paciente')
+
+df_paciente_atendimento = df_paciente_atendimento.dropna(
+    subset='especialidade')
+
 # Remove as linhas 2 e 3 que estão com valores corrompidos
 df_paciente_atendimento = df_paciente_atendimento.drop([1, 2])
+
 # Contagem de exames mais feitos
 contagem_exames = df_paciente_atendimento['especialidade'].value_counts()
 
 # Cria e mistura as duas df, tira a coluna com nomes de pacientes e salva para uso posterior
-df_especialidade_contagem = pd.DataFrame()
 df_especialidade_contagem = df_paciente_atendimento.merge(
     contagem_exames.to_frame('contagem').reset_index(), on='especialidade')
 df_especialidade_contagem = df_especialidade_contagem.drop(
     'nome do paciente', axis=1)
+# Removendo a duplicação de linhas na coluna especialidade
+df_sem_dup = df_especialidade_contagem.drop_duplicates(subset='especialidade', keep='first')
+
+print(df_sem_dup)
